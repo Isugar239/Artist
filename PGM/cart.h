e1eb9d37142cc9f10970779e37e612d6fbe24062
Ivan <163177546+Isugar239@users.noreply.github.com>
2025-12-29 10:48:44 +0000

    понаписал классов тележки с 2 моторами и маркера

---
+++ PGM/cart.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <GyverStepper2.h>
+
+class cart {
+public:
+  GStepper2<STEPPER2WIRE> left;
+  GStepper2<STEPPER2WIRE> right;
+
+  cart(int res,
+       int l_dir, int l_step,
+       int r_dir, int r_step,
+       int maxSpeed = 648,
+       int maxAccel = 500)
+    : left(res, l_dir, l_step),
+      right(res, r_dir, r_step)
+  {
+    left.setRunMode(FOLLOW_POS);
+    right.setRunMode(FOLLOW_POS);
+
+    left.setMaxSpeed(maxSpeed);
+    right.setMaxSpeed(maxSpeed);
+
+    left.setAcceleration(maxAccel);
+    right.setAcceleration(maxAccel);
+  }
+
+  void setPos(int pos) {
+    left.setTarget(pos);
+    right.setTarget(pos);
+  }
+
+  bool tick() {
+    return left.tick() | right.tick();
+  }
+
+  void gotoPos(int pos) {
+    this->setPos(pos);
+    while (tick()) {}
+  }
+};
--- PGM/marker.h
+++ PGM/marker.h
@@ -1,27 +1,33 @@
+#pragma once
 #include <Servo.h>
-#include<stepper.h>
-Class marker
-{
+#include "cart.h"
+
+class marker {
 private:
-  int pin;
+  Servo marker;
   int posUp;
   int posDown;
+  cart& robot;
 
 public:
-  marker(int pin, int posUp, int posDown)
-  {
-    Servo marker;
+  marker(int pin, int up, int down, cart& c)
+    : posUp(up), posDown(down), robot(c) {
     marker.attach(pin);
-  };
-  void down()
-  {
-    marker.write(posDown);
-  }
-  void up()
-  {
     marker.write(posUp);
   }
-  void drawLine(int start, int stop){
 
+  void up() {
+    marker.write(posUp);
+  }
+
+  void down() {
+    marker.write(posDown);
+  }
+
+  void drawLine(int posStart, int posStop) {
+    robot.gotoPos(posStart);
+    this->down();
+    robot.gotoPos(posStop);
+    this->up();
   }
 };
